368595c36295c11ae1514a9d9e37916d8d9bdb18
Gisele <gisesonia@gmail.com>
2023-04-14 19:19:27 -0300

    ajustes em tutores

--- src/routes/tutoresRoutes.js
+++ src/routes/tutoresRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import UsuariosController from "../controllers/UsuariosController.js";
 import { verificarToken } from "../middlewares/verificarToken.js";
-import { verificarTipoRecurso } from "../middlewares/verificarTipoRecurso.js";
+
 
 const router = express.Router();
 
 router.get("/tutores", UsuariosController.listarTutores);
 router.get("/tutores/:id", UsuariosController.listarTutorPorId);
-router.post("/tutores",UsuariosController.cadastrarUsuario);
-router.put("/tutores/:id",verificarToken,verificarTipoRecurso("Tutor"),UsuariosController.atualizarUsuario);
-router.delete("/tutores/:id", verificarToken, verificarTipoRecurso("Tutor"), UsuariosController.excluirUsuario);
+router.post("/tutores", UsuariosController.cadastrarUsuario);
+router.put("/tutores/:id",verificarToken,UsuariosController.atualizarUsuario);
+router.delete("/tutores/:id", verificarToken, UsuariosController.excluirUsuario);
 
 export default router;
