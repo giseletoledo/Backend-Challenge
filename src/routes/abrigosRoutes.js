4b6587e5a331264ccaf46ff973044befdeff7433
Gisele <gisesonia@gmail.com>
2023-04-14 19:19:38 -0300

    rotas de abrigos

--- src/routes/abrigosRoutes.js
+++ src/routes/abrigosRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import UsuariosController from "../controllers/UsuariosController.js";
 import { verificarToken } from "../middlewares/verificarToken.js";
-import { verificarTipoRecurso } from "../middlewares/verificarTipoRecurso.js";
+
+
 
 const router = express.Router();
 
 router.get("/abrigos/", UsuariosController.listarAbrigos);
 router.get("/abrigos/:id", UsuariosController.listarAbrigoPorId);
 router.post("/abrigos", UsuariosController.cadastrarUsuario);
-router.put("/abrigos/:id", verificarToken,verificarTipoRecurso("Abrigo"),UsuariosController.atualizarUsuario);
-router.delete("/abrigos/:id",verificarToken,verificarTipoRecurso("Abrigo"), UsuariosController.excluirUsuario);
+router.put("/abrigos/:id", verificarToken,UsuariosController.atualizarUsuario);
+router.delete("/abrigos/:id",verificarToken, UsuariosController.excluirUsuario);
 
 export default router;
